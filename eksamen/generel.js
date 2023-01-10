80e17d0fe99cfd54e682a8ab85c50b2a112d0807
magnuslykke <119849826+magnuslykke@users.noreply.github.com>
2023-01-10 16:54:32 +0100

    Add files via upload

---
+++ eksamen/generel.js
@@ -0,0 +1,27 @@
+//----Når siden er loaded, viser jeg funktionen sidenVises---//
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
